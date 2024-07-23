fad46bd2e8660593469db58f0e5c1bd67a15de7f
sourabh1905 <1905gurjar@gmail.com>
2024-07-23 15:21:06 +0530

    fix the build issue 2

--- server/server.js
+++ server/server.js
@@ -15,14 +15,9 @@ app.get('*',(req,res)=>
   res.sendFile(path.join(__dirname,"/Client/dist/index.html"))
 )
 const port = process.env.PORT || 3000;
-const uri ='mongodb+srv://sourabh19052003:414958%23IITM@care.keyd5dd.mongodb.net/?retryWrites=true&w=majority&appName=care' ;
-
+const uri =process.env.MONGODB_URI;
 
 mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,
-  socketTimeoutMS: 45000,
 })
 .then(() => {
   console.log('Successfully connected to MongoDB Atlas!');
